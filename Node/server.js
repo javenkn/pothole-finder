05a064e92913a9736ac1ca769cabbc4659b72273
Christie Reindle <creindle@hawaii.edu>
2017-03-11 22:59:14 +0000

    Add files via upload

--- Node/server.js
+++ Node/server.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 const public = path.join(__dirname, 'public');
+const expressWs = require('express-ws')(app);
+const mercury = require('./public/js/mercury_switch.js'); 
+let coordinates = [];
 
+//console.log(coords);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static('public')); // for static image and css files
@@ -15,6 +19,28 @@ app.get('/admin', function (req, res) {
   res.render('admin');
 });
 
-var server = app.listen(PORT, function () {
+// listen for websocket connections on the root URI
+app.ws('/', function(ws, req) {
+  //mercury();
+  // Event Listener waiting for a message to come in over the socket connection
+  ws.on('message', (coords) => {
+    //console.log(coords);
+    JSON.parse(coords);
+    coordinates.push(coords);
+    
+    console.log(coordinates);
+    console.log(`received: ${coordinates}`);
+    mercury(ws);
+  });
+
+  // Event Listener waiting for the connection to close
+  ws.on('end', () => {
+    console.log('Connection ended...');
+  });
+
+  mercury(ws);
+});
+
+app.listen(PORT, () => {
   console.log(`Now listing on Port ${PORT}`);
 })
