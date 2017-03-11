cef54f7ff47285f9a87edffbc47339136a564c4b
Jay Won <jason@sudokrew.com>
2017-03-11 13:30:29 -1000

    :fire: Cleaning up some comments

--- Node/public/js/app.js
+++ Node/public/js/app.js
@@ -3,7 +3,6 @@ console.log('open: ');
 const ws = new WebSocket("ws://localhost:3000/");
 ws.onopen = function (event) {
   console.log('Connection is open ...');
- // ws.send("Hello Server");
 };
 
 ws.onerror = function (err) {
@@ -11,18 +10,17 @@ ws.onerror = function (err) {
 };
 
 ws.onmessage = function (event) {
-  console.log("Yes"); 
+  console.log("Yes");
   navigator.geolocation.getCurrentPosition(showPosition);
 };
 
 function showPosition(position) {
   var coords = position.coords;
- // console.log(ws);
   console.log(coords.latitude);
   console.log(coords.longitude);
   ws.send(JSON.stringify({
-	lat: coords.latitude,
-	long: coords.longitude
+	  lat: coords.latitude,
+	  long: coords.longitude
   }));
   myMap(coords.latitude, coords.longitude);
 }
@@ -33,7 +31,7 @@ function myMap(lat, long){
       zoom: 17,
       mapTypeId: google.maps.MapTypeId.HYBRID
    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
 
