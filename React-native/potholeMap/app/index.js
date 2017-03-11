959a0cd06c8d14785674e9fe1e48ada6829d54a2
MartaKatona <marta.katona.us@gmail.com>
2017-03-11 04:46:08 -1000

    restructure code, all js file in app folder
    set state in index.js
    set onchange function that updates this.state.region by onRegionChange function
    text under map is updated according to state

---
+++ React-native/potholeMap/app/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+// import MapView, { Marker } from 'react-native-maps';
+import MapView from 'react-native-maps';
+
+import Geolocation from './Geolocation.js';
+
+let {height, width} = Dimensions.get('window');
+
+export default class potholeMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 21.3069,
+        longitude: -157.8583,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    };
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+        </MapView>
+        <View style={styles.container}>
+          <Text>
+            MAP DEMO !!!
+            Latitude: {this.state.region.latitude}{'\n'}
+            Longitude: {this.state.region.longitude}{'\n'}
+            LatitudeDelta: {this.state.region.latitudeDelta}{'\n'}
+            LongitudeDelta: {this.state.region.longitudeDelta}
+          </Text>
+        </View>
+     </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  map: {
+    // flex: 1
+    width: width,
+    height: height*2/3
+  }
+});
--- React-native/potholeMap/index.ios.js
+++ React-native/potholeMap/index.ios.js
@@ -5,52 +5,7 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-// import MapView, { Marker } from 'react-native-maps';
-import MapView from 'react-native-maps';
-
-import Geolocation from './Geolocation.js';
-
-export default class potholeMap extends Component {
-
-  render() {
-    return (
-      <View
-        style={styles.container}
-      >
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 21.3069,
-            longitude: -157.8583,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-        </MapView>
-        <Text>
-          Map Demo!!!
-        </Text>
-        <Geolocation />
-     </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  map: {
-    flex: 1
-  }
-});
+import { AppRegistry } from 'react-native';
+import potholeMap from './app';
 
 AppRegistry.registerComponent('potholeMap', () => potholeMap);
